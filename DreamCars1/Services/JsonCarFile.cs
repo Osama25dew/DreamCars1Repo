74cda96cf3145ac778144eba5f2db9eff3928a55
Honey <honeymaarouf@gmail.com>
2022-10-30 11:05:14 -0700

    third commit

---
+++ DreamCars1/Services/JsonCarFile.cs
@@ -0,0 +1,28 @@
+﻿using DreamCars1.Models;
+using System.Text.Json;
+
+namespace DreamCars1.Services
+{
+    public class JsonCarFile
+    {
+        public JsonCarFile(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+        public IWebHostEnvironment WebHostEnvironment { get; }
+        public string JsonFilePath
+        {
+            get
+            {
+                return Path.Combine(WebHostEnvironment.WebRootPath, "data", "dreamCars1.json");
+            }
+        }
+        public IEnumerable<Car> getCarsData()
+        {
+            using(var jsonFile = File.OpenText(JsonFilePath))
+            {
+                return JsonSerializer.Deserialize<Car[]>(jsonFile.ReadToEnd()); 
+            }
+        }
+    }
+}
