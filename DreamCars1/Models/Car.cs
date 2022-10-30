17a836c54f3581c12ea67b52e0b28063d42fa0a7
Honey <honeymaarouf@gmail.com>
2022-10-29 22:57:49 -0700

    second commit

---
+++ DreamCars1/Models/Car.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace DreamCars1.Models
+{
+    public class Car
+    {
+        [JsonPropertyName("id")] //if we are using same name in both json and in class...then we dont need to sync jsonpropertyname
+        public int carId { get; set; }
+       
+        [JsonPropertyName("name")]
+        public string carName { get; set; }
+
+        [JsonPropertyName("img")]
+        public string carImg { get; set; }
+
+        public override string ToString() //yani jab b mai ToString fn ko call karoon ga to hamesha yahi fn call ho ga bcz mai ne isse override kiya (fnoverriding means k same fn likha jaye with same parameters)
+        {
+            return JsonSerializer.Serialize<Car>(this); //yeh hum is liye use kar rahe k jo json file specific format mai hai isse hum string format mai change kar sake or <Car> se matlab yeh hai k hum <Car> k hisab se future mai serialize kare ge or "this" is liye k jon sa b current obj ho ga uusee.. 
+        }
+    }
+}
